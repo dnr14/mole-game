f7048d3e98e1faf4e688313305473c294e754caf
minwook Lee <dnr14@naver.com>
2022-02-20 03:07:31 +0900

    Add : Readme 작성

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# whack-a-mole-game
+# 1. 두더지 잡기 게임
+
+### 🐭 JS 문법 공부하기 위해 두더지 잡기 게임을 개발하였습니다.
+
+- 4 X 4 판 위에 무작위로 나오는 🐭카드를 클릭 시 점수가 오른다.
+- 60초안에 점수를 많이 획득한 유저가 승리한다.
+- [게임체험하기](https://dnr14.github.io/mole)
+
+<div align=center>
+  <img src=./images/mole.gif />
+</div>
--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-const $div = document.querySelectorAll('.root .item');
-const $mole = document.querySelectorAll('.root .item .mole');
-const $boom = document.querySelectorAll('.root .item .boom');
-const $score = document.querySelector('.score')
+const $div = document.querySelectorAll(".root .item");
+const $mole = document.querySelectorAll(".root .item .mole");
+const $boom = document.querySelectorAll(".root .item .boom");
+const $score = document.querySelector(".score");
+const $time = document.querySelector(".time");
+
 let _globalcount = 0;
 
 eventHandler($mole, true);
@@ -10,31 +12,29 @@ eventHandler($boom, false);
 function eventHandler(currentEl, y) {
   currentEl.forEach((el) => {
     el.isclick = false;
-    el.addEventListener('click', scoreCheck(y));
+    el.addEventListener("click", scoreCheck(y));
   });
 }
 
 function scoreCheck(y) {
   return (e) => {
-
     if (e.currentTarget.isclick == true) {
       return e.preventDefault();
     }
-    y == true ? _globalcount += 1 : _globalcount -= 1;
+    y == true ? (_globalcount += 1) : (_globalcount -= 1);
     e.currentTarget.isclick = true;
     e.currentTarget.classList.remove("on");
     $score.innerText = `${_globalcount}점`;
-  }
+  };
 }
 
 const remove = (v, index) => () => {
   $div[v].children[index].classList.remove("on");
   $div[v].children[index].isclick = false;
   $div[v].ishide = true;
-}
+};
 
 const timer = setInterval(() => {
-
   for (let i = 0; i < 5; i++) {
     let random = rand(0, 8);
     let index = 0;
@@ -52,15 +52,21 @@ const timer = setInterval(() => {
     let hideTime = rand(2, 3) * 1000 - 500;
     setTimeout(remove(random, index), hideTime);
   }
-
 }, 1000);
 
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// 끝내는 로직
-// setTimeout(() => {
-//   clearInterval(timer);
-//   alert("끝");
-// }, 10000);
+const END_TIME = 60;
+let time = 1;
+
+const currentTime = setInterval(() => {
+  $time.innerText = `${END_TIME - time}초`;
+  if (time === END_TIME) {
+    clearInterval(timer);
+    clearInterval(currentTime);
+    alert("끝");
+  }
+  time += 1;
+}, 1000);
